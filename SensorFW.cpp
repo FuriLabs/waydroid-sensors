8ae8d0cc869ab0bff576715bf1ca5f98df375993
Bardia Moshiri <bardia@furilabs.com>
2025-04-16 13:13:57 -0400

    Sensors: implement linear acceleration sensor
    sensorfw also squashes these two on binder. we can do the same here
    
    Signed-off-by: Bardia Moshiri <bardia@furilabs.com>

--- SensorFW.cpp
+++ SensorFW.cpp
@@ -94,9 +94,11 @@ SensorFW::SensorFW()
     try {
         data->accelerometer_sensor = std::make_shared<andromeda::core::SensorfwAccelerometerSensor>(dbus_address);
         data->sensorAvailable[ID_ACCELEROMETER] = TRUE;
+        data->sensorAvailable[ID_LINEAR_ACCELERATION] = TRUE;
     } catch (std::exception const &e) {
         GINFO("Failed to create SensorfwAccelerometerSensor: %s", e.what());
         data->sensorAvailable[ID_ACCELEROMETER] = FALSE;
+        data->sensorAvailable[ID_LINEAR_ACCELERATION] = FALSE;
     }
     try {
         data->gyroscope_sensor = std::make_shared<andromeda::core::SensorfwGyroscopeSensor>(dbus_address);
@@ -172,6 +174,7 @@ void SensorFW::RegisterSensors(sensor_event_cb_t cb, void *userdata) {
                 [this, cb, userdata](AccelerationData value) {
                     this->data->accelerometer_event = value;
                     cb(userdata, ID_ACCELEROMETER);
+                    cb(userdata, ID_LINEAR_ACCELERATION);
                 }));
     }
     if (data->sensorAvailable[ID_GYROSCOPE]) {
@@ -270,6 +273,9 @@ int SensorFW::EnableSensorEvents(int id) {
     case ID_ACCELEROMETER:
         data->accelerometer_sensor->enable_accelerometer_events();
         break;
+    case ID_LINEAR_ACCELERATION:
+        data->accelerometer_sensor->enable_accelerometer_events();
+        break;
     case ID_GYROSCOPE:
         data->gyroscope_sensor->enable_gyroscope_events();
         break;
@@ -315,6 +321,9 @@ int SensorFW::DisableSensorEvents(int id) {
 
     switch (id) {
     case ID_ACCELEROMETER:
+        data->accelerometer_sensor->disable_accelerometer_events();
+        break;
+    case ID_LINEAR_ACCELERATION:
         data->accelerometer_sensor->disable_accelerometer_events();
         break;
     case ID_GYROSCOPE:
